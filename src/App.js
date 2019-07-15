dfe5d4d6a783032de3d859d8f751a2dbfa041c1d
Siwoo <postsw7@gmail.com>
2019-07-15 19:27:22 -0400

    Update App.js by rendering images along with following metadata and adjust style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './App.scss';
+import { useRandomPhotos } from './utils/hooks';
 
 function App() {
+  const randomPhotos = useRandomPhotos();
+
   return (
     <div className='App'>
       <div>Random Picture List</div>
+      <div className='columns is-multiline'>
+        {randomPhotos.map(photo => {
+          return (
+            <div className='column is-one-fifth' key={photo.id}>
+              <figure>
+                <img src={photo.urls.thumb} alt={photo.alt_description} />
+                <figcaption>
+                  {photo.description}
+                  <p>Photographer: {photo.user.name}</p>
+                  <p>Downloads: {photo.downloads}</p>
+                  <p>Likes: {photo.likes}</p>
+                </figcaption>
+              </figure>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
